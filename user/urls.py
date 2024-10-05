0f7efb92f8e9ac19703d623592023773cec223f9
JonnyFernandez <arcancode@gmail.com>
2024-10-05 12:33:50 -0300

    updating architecture

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import signUp, signIn, signOut
-
-urlpatterns = [
-    path("signin/", signIn),
-    path("signup/", signUp),
-    path("signout/", signOut),
-]
