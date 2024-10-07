4acbf9ff6c941d2c09495c1b957ff26fe359cd07
JonnyFernandez <arcancode@gmail.com>
2024-10-07 01:49:41 -0300

    cloudinary integrado

--- commerce/models.py
+++ commerce/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image_url = models.URLField(max_length=200)
+    imagen = CloudinaryField("image", blank=True, null=True)
     precio = models.DecimalField(
         max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)]
     )
     stock = models.IntegerField(validators=[MinValueValidator(0)])
-    created = models.DateTimeField(null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    oferta = models.BooleanField(default=False)
+    activo = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return self.name
