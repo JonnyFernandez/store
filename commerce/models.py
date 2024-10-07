d0b033d04e9df95a6dd76e8b514fd4cc83989577
JonnyFernandez <arcancode@gmail.com>
2024-10-07 17:55:43 -0300

    cart

--- commerce/models.py
+++ commerce/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 from cloudinary.models import CloudinaryField
 
 
-# Create your models here.
+# Productos
 class Product(models.Model):
     nombre = models.CharField(max_length=100)
     description = models.TextField(blank=True)
@@ -19,3 +19,65 @@ class Product(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+# Carrito
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    items = models.ManyToManyField(
+        "Product", through="CartItem"
+    )  # Relación con los productos a través de CartItem
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Carrito de {self.user.username}"
+
+    # Método para calcular el costo total del carrito
+    def get_total_cost(self):
+        return sum(item.get_total_price() for item in self.cartitem_set.all())
+
+
+# Item del Carrito
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+    def get_total_price(self):
+        return self.quantity * self.product.precio
+
+
+# Orden (Historial de Compras)
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders"
+    )  # Relación de uno a muchos
+    items = models.ManyToManyField(
+        "Product", through="OrderItem"
+    )  # Relación con los productos comprados
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[("Pending", "Pending"), ("Completed", "Completed")],
+        default="Pending",
+    )
+
+    def __str__(self):
+        return f"Orden {self.id} de {self.user.username}"
+
+
+# Item de la Orden
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2
+    )  # Precio en el momento de la compra
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} en orden {self.order.id}"
--- commerce/urls.py
+++ commerce/urls.py
@@ -1,10 +1,32 @@
 from django.urls import path
-from .views import landing_page, home, cart, about, card_detail
+from .views import (
+    landing_page,
+    home,
+    view_cart,
+    about,
+    card_detail,
+    add_to_cart,
+    increment_cart_item,
+    decrement_cart_item,
+    remove_cart_item,
+)
 
 urlpatterns = [
     path("", landing_page, name="landing"),
     path("home/", home, name="home"),
-    path("cart/", cart, name="cart"),
+    path("cart/", view_cart, name="cart"),
     path("about/", about, name="about"),
     path("card_detail/<int:prod_id>", card_detail, name="card_detail"),
+    path("add_to_cart/<int:product_id>", add_to_cart, name="add_to_cart"),
+    path(
+        "increment_cart_item/<int:cart_item_id>",
+        increment_cart_item,
+        name="incrementar",
+    ),
+    path(
+        "decrement_cart_item/<int:cart_item_id>",
+        decrement_cart_item,
+        name="decrementar",
+    ),
+    path("remove_cart_item/<int:cart_item_id>", remove_cart_item, name="remove"),
 ]
